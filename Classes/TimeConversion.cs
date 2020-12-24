f67d41f468276ec1956814ae13bfd04af4289411
abmusawer <abmusawer.npa@gmail.com>
2020-12-24 19:47:46 +0430

    time conversion challenge done

---
+++ Classes/TimeConversion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Classes
+{
+    public static class TimeConversion
+    {
+        public static string timeConversion(string s)
+        {
+            DateTime dateTime = DateTime.Parse(s);
+            return dateTime.ToString("HH:mm:ss");
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,9 +9,13 @@ namespace HackerRank
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
+
             //Console.WriteLine(Mini_Max_Sum.MainFunc());
 
-            Console.WriteLine(BirthdayCakeCandles.birthdayCakeCandles(new List<int> { 3,1,11,3}));
+            //Console.WriteLine(BirthdayCakeCandles.birthdayCakeCandles(new List<int> { 3,1,11,3}));
+
+            Console.WriteLine(TimeConversion.timeConversion("07:05:45PM"));
+
             Console.Read();
         }
     }
